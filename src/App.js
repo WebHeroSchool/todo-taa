bf2e244e16cbcff361fe5d235e2a15c4d56962ac
Антон Ткачук <chiga2030@gmail.com>
2020-12-13 14:23:43 +0300

    added examples of rendering

--- src/App.js
+++ src/App.js
@@ -1,22 +1,67 @@
 import logo from './logo.svg';
 import './App.css';
 
+// вывод объекта style:
+const parStyle = {
+  backgroundColor: 'red',
+  fontSize: '2em',
+}
+
+// вывод значения переменной:
+const parText = 'my first react app';
+
+// вывод результата логической операции:
+const bool = () => {
+  if(true) {
+    return 'будет выведена в разметку';
+  } return 'НЕ будет выведена в разметку';
+}
+
+// пример с тернарным оператором:
+const isAuth = false;
+
+// примеры undefined, null, false и true:
+function UserGreeting(props) {
+  return 'С возвращением!';
+}
+
+function GuestGreeting(props) {
+  return 'Войдите, пожалуйста.';
+}
+
+function Greeting(props) {
+  const isLoggedIn = props.isLoggedIn;
+  if (isLoggedIn) {
+    return <UserGreeting />;
+  } return <GuestGreeting />;
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p style ={parStyle}>
+          {parText}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          Пример вывода числа в разметку: {42}
+        </p>
+        <p>
+          Два плюс два, будет {2 + 2}
+        </p>
+        <p>
+          Логическая операция которая {bool()}
+        </p>
+        <p>
+          Пример использования тернарного оператора:
+          <br/>
+          Пользователь {isAuth ? 'авторизован' : 'НЕ авторизован'} на сайте
+        </p>
+        <h2>Примеры undefined, null, false и true</h2>
+        <h3>TRUE: <Greeting isLoggedIn={true} /></h3>
+        <h3>FALSE: <Greeting isLoggedIn={false} /></h3>
+        <h3>NULL: <Greeting isLoggedIn={null} /></h3>
+        <h3>UNDEFINED: <Greeting isLoggedIn={undefined} /></h3>
       </header>
     </div>
   );

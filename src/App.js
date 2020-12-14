276c3e5da2089b82f24ccdfe6c2389d6044cfb0e
Anton Tkachuk <tkachuk.ant@mail.ru>
2020-12-14 08:44:33 +0300

    added import/export example

--- src/App.js
+++ src/App.js
@@ -1,40 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import { count, length } from './modules/number.js';
 
 // вывод объекта style:
 const parStyle = {
   backgroundColor: 'red',
   fontSize: '2em',
 }
-
 // вывод значения переменной:
 const parText = 'my first react app';
-
-// вывод результата логической операции:
-const bool = () => {
-  if(true) {
-    return 'будет выведена в разметку';
-  } return 'НЕ будет выведена в разметку';
-}
-
-// пример с тернарным оператором:
-const isAuth = false;
-
-// примеры undefined, null, false и true:
-function UserGreeting(props) {
-  return 'С возвращением!';
-}
-
-function GuestGreeting(props) {
-  return 'Войдите, пожалуйста.';
-}
-
-function Greeting(props) {
-  const isLoggedIn = props.isLoggedIn;
-  if (isLoggedIn) {
-    return <UserGreeting />;
-  } return <GuestGreeting />;
-}
 
 function App() {
   return (
@@ -44,24 +17,8 @@ function App() {
           {parText}
         </p>
         <p>
-          Пример вывода числа в разметку: {42}
+          Произведение count * length = {count * length}
         </p>
-        <p>
-          Два плюс два, будет {2 + 2}
-        </p>
-        <p>
-          Логическая операция которая {bool()}
-        </p>
-        <p>
-          Пример использования тернарного оператора:
-          <br/>
-          Пользователь {isAuth ? 'авторизован' : 'НЕ авторизован'} на сайте
-        </p>
-        <h2>Примеры undefined, null, false и true</h2>
-        <h3>TRUE: <Greeting isLoggedIn={true} /></h3>
-        <h3>FALSE: <Greeting isLoggedIn={false} /></h3>
-        <h3>NULL: <Greeting isLoggedIn={null} /></h3>
-        <h3>UNDEFINED: <Greeting isLoggedIn={undefined} /></h3>
       </header>
     </div>
   );

79991119f9e6b43a97e920fcde84484713f6a4d7
Anton Tkachuk <tkachuk.ant@mail.ru>
2020-12-14 11:55:19 +0300

    clean App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,6 @@
-import './App.css';
-import { count, length } from './modules/number.js';
-
-// вывод объекта style:
-const parStyle = {
-  backgroundColor: 'red',
-  fontSize: '2em',
-}
-// вывод значения переменной:
-const parText = 'my first react app';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p style ={parStyle}>
-          {parText}
-        </p>
-        <p>
-          Произведение count * length = {count * length}
-        </p>
-      </header>
-    </div>
+    
   );
 }
 

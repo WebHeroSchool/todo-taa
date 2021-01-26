2948ec8daf5476294657bf5de21f0ab9dc781ffc
Anton Tkachuk <chiga2030@gmail.com>
2021-01-26 09:33:42 +0300

    import and use classnames lib

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,5 +1,13 @@
-const Item = ({ value }) => {
-  return (<li>
+import classnames from 'classnames';
+import styles from './Item.module.css';
+
+const Item = ({ value, isDone }) => {
+  return (<li className = {
+    classnames({
+      [styles.item]: true,
+      [styles.done]: isDone,
+    })
+  }>
     { value }
   </li>);
 }

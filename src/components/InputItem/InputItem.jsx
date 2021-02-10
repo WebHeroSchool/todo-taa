82907f5b717be6c0bf1147cfc67c2d271fa436fa
Anton Tkachuk <chiga2030@gmail.com>
2021-02-10 11:21:04 +0300

    functionality for adding new tasks

--- src/components/InputItem/InputItem.jsx
+++ src/components/InputItem/InputItem.jsx
@@ -1,13 +1,47 @@
-import TextField from '@material-ui/core/TextField';
+import React from 'react';
+import {
+  TextField,
+  Button,
+} from '@material-ui/core';
+import PostAddIcon from '@material-ui/icons/PostAdd';
 import styles from './InputItem.module.css';
 
-const InputItem = () => <TextField
-  className={styles.indent}
-  id="outlined-basic"
-  fullWidth={true}
-  margin="dense"
-  label="Добавить дело"
-  variant="outlined"
-/>;
+class InputItem extends React.Component {
+  state = {
+    inputValue: '',
+  }
+
+  onButtonClick = () => {
+    this.setState({
+      inputValue: '',
+    });
+
+    this.props.onClickAddItem(this.state.inputValue);
+  }
+
+  render () {
+    return (<div className={styles.wrapper}>
+      <TextField
+        className={styles.textField}
+        fullWidth={true}
+        id="outlined-basic"
+        label="Добавить дело"
+        margin="dense"
+        value={this.state.inputValue}
+        variant="outlined"
+        onChange={event => this.setState({
+          inputValue: event.target.value,
+        })}
+      />
+      <Button
+        className={styles.button}
+        color="primary"
+        onClick={this.onButtonClick}
+      >
+        <PostAddIcon fontSize="default" />
+      </Button>
+    </div>);
+  }
+}
 
 export default InputItem;

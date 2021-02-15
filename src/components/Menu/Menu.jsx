30d6830fffa966bc2f2196b026ec763ca5c23249
Anton <chiga2030@gmail.com>
2021-02-15 23:25:19 +0300

    added menu

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,49 +1,67 @@
+import {
+  React,
+  useState,
+} from 'react';
+import {
+  Link,
+} from 'react-router-dom';
 import styles from './Menu.module.css';
 import {
   Paper,
   MenuList,
+  ListItem,
+  ListItemText,
 } from '@material-ui/core';
-import {
-  Link,
-} from 'react-router-dom';
-
-import MyMenuItem from '../MyMenuItem/MyMenuItem';
 
 const Menu = () => {
-  const menuItems = [
-    {
-      value: 'Обо мне',
-      path: '/',
-    },
-    {
-      value: 'Дела',
-      path: '/todo',
-    },
-    {
-      value: 'Контакты',
-      path: '/contacts',
-    },
-  ];
+  const [
+    selectedIndex,
+    setSelectedIndex,
+  ] = useState(0);
+
+  const handleListItemClick = (event, index) => {
+    setSelectedIndex(index);
+  };
 
 
   return (
     <Paper className={styles.menu} elevation={3} >
       <MenuList>
 
-        {
-          menuItems.map(menuItem => (
-            <Link
-              key={menuItem.value.toString()}
-              to={menuItem.path}
-            >
-              <MyMenuItem value={menuItem.value} />
-            </Link>
-          ))
-        }
+        <Link to='/'>
+          <ListItem
+            button
+            selected={selectedIndex === 1}
+            onClick={event => handleListItemClick(event, 1)}
+          >
+            <ListItemText primary="Обо мне" />
+          </ListItem>
+        </Link>
+
+        <Link to='/todo'>
+          <ListItem
+            button
+            selected={selectedIndex === 2}
+            onClick={event => handleListItemClick(event, 2)}
+          >
+            <ListItemText primary="Дела" />
+          </ListItem>
+        </Link>
+
+        <Link to='/contacts'>
+          <ListItem
+            button
+            selected={selectedIndex === 3}
+            onClick={event => handleListItemClick(event, 3)}
+          >
+            <ListItemText primary="Контакты" />
+          </ListItem>
+        </Link>
 
       </MenuList>
     </Paper>
   );
 };
+
 
 export default Menu;

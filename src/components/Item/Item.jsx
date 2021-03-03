f18db134e2e14ea2702174cd4d8e425a9c6f38d3
Anton Tkachuk <chiga2030@gmail.com>
2021-03-03 08:55:02 +0300

    back to hocless

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,20 +1,42 @@
 import classnames from 'classnames';
 import styles from './Item.module.css';
+import {
+  Button,
+  FormControlLabel,
+  Checkbox,
+} from '@material-ui/core';
+import DeleteForeverTwoToneIcon from '@material-ui/icons/DeleteForeverTwoTone';
 import PropTypes from 'prop-types';
 
 
 const Item = props => (
-  <span
-    onClick = {() => props.updateItem(props.id)}
-    className = {
+  <div className = {styles.itemWrap}>
+    <span className = {
       classnames({
         [styles.item]: true,
         [styles.done]: props.isDone,
       })
-    }
-  >
-    { props.value }
-  </span>
+    }>
+      <FormControlLabel
+        control={
+          <Checkbox
+            onChange={() => props.updateItem(props.id)}
+            checked={props.isDone}
+            name={`checked ${props.value.toString()}`}
+          />
+        }
+        label={ props.value }
+      />
+      <Button
+        className={styles.deleteBtn}
+        variant="contained"
+        color="secondary"
+        onClick={() => props.deleteItem(props.id)}
+      >
+        <DeleteForeverTwoToneIcon fontSize="default" />
+      </Button>
+    </span>
+  </div>
 );
 
 Item.propTypes = {
@@ -22,7 +44,7 @@ Item.propTypes = {
   value: PropTypes.string,
   isDone: PropTypes.bool,
   updateItem: PropTypes.func,
-  // deleteItem: PropTypes.func,
+  deleteItem: PropTypes.func,
 };
 
 export default Item;

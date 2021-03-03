9c792fe511c490095bc0fb2f1500ef35954ed8e9
Anton Tkachuk <chiga2030@gmail.com>
2021-03-03 09:41:54 +0300

    refactoring HOC withItemList

--- src/components/hoc/withItemList/withItemList.jsx
+++ src/components/hoc/withItemList/withItemList.jsx
@@ -1,31 +1,20 @@
 import styles from './withItemList.module.css';
-import PropTypes from 'prop-types';
 
-const withItemList = Component => ({
-  items,
-  updateItem,
-  deleteItem,
-}) => (
+
+const withItemList = Component => props => (
   <ul className={ styles.list }>
-    {items.map(item => (
-      <li key={item.id.toString()}>
-        <Component
-          id={item.id}
-          value={item.value}
-          isDone={item.isDone}
-          updateItem={updateItem}
-          deleteItem={deleteItem}
-        />
-      </li>)
+    {props.items.map(
+      item => (
+        <li key={item.id.toString()}>
+          <Component
+            {...item}
+            {...props}
+          />
+        </li>
+      )
     )}
   </ul>
 );
 
-withItemList.propTypes = {
-  className: PropTypes.string,
-  items: PropTypes.array,
-  onClickIsDone: PropTypes.func,
-  onClickDelete: PropTypes.func,
-};
 
 export default withItemList;

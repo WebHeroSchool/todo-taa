6723266dc0762d44234f251f10ab75d485b91e4c
Anton <chiga2030@gmail.com>
2021-08-31 23:28:09 +0300

    filters for items completed

--- src/components/FilterListItems/FilterListItems.jsx
+++ src/components/FilterListItems/FilterListItems.jsx
@@ -8,12 +8,18 @@ import PropTypes from 'prop-types';
 const FilterListItems = props => (
   <div className={styles.filtrWrapper}>
     <ButtonGroup size="small" aria-label="small outlined button group">
-      <Button className={styles.buttonText}>Все</Button>
+      <Button
+        className={styles.buttonText}
+        onClick={() => props.setAllFilter()}
+      >Все</Button>
       <Button
         className={styles.buttonText}
         onClick={() => props.setActiveFilter()}
       >Активные</Button>
-      <Button className={styles.buttonText}>Выполненные</Button>
+      <Button
+        className={styles.buttonText}
+        onClick={() => props.setComplitedFilter()}
+      >Выполненные</Button>
     </ButtonGroup>
   </div>
 );
---
+++ src/store/actions/todo/setAllFilter.js
@@ -0,0 +1,6 @@
+export const SET_ALL_FILTER = 'SET_ALL_FILTER';
+
+export const setAllFilter = () => ({
+  type: SET_ALL_FILTER,
+  payload: 'all',
+});

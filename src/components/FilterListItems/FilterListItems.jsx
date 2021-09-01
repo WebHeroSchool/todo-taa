c7e558749c5b30fdbef05ccb844f2745248a19a9
Anton <chiga2030@gmail.com>
2021-09-01 22:47:47 +0300

    added toggle buttons

--- src/components/FilterListItems/FilterListItems.jsx
+++ src/components/FilterListItems/FilterListItems.jsx
@@ -1,28 +1,65 @@
+import {
+  useState,
+} from 'react';
 import styles from './FilterListItems.module.css';
 import {
-  ButtonGroup,
-  Button,
-} from '@material-ui/core';
+  ToggleButton,
+  ToggleButtonGroup,
+} from '@material-ui/lab';
+
 import PropTypes from 'prop-types';
 
-const FilterListItems = props => (
-  <div className={styles.filtrWrapper}>
-    <ButtonGroup size="small" aria-label="small outlined button group">
-      <Button
-        className={styles.buttonText}
-        onClick={() => props.setAllFilter()}
-      >Все</Button>
-      <Button
-        className={styles.buttonText}
-        onClick={() => props.setActiveFilter()}
-      >Активные</Button>
-      <Button
-        className={styles.buttonText}
-        onClick={() => props.setComplitedFilter()}
-      >Выполненные</Button>
-    </ButtonGroup>
-  </div>
-);
+
+const FilterListItems = props => {
+  const [
+    alignment,
+    setAlignment,
+  ] = useState('left');
+
+  const handleAlignment = (event, newAlignment) => {
+    setAlignment(newAlignment);
+  };
+
+
+  return (
+    <div className={styles.filtrWrapper}>
+      <ToggleButtonGroup
+        size="small"
+        value={alignment}
+        exclusive
+        onChange={handleAlignment}
+        aria-label="text alignment"
+      >
+        <ToggleButton
+          color="primary"
+          value="left"
+          aria-label="left aligned"
+          className={styles.buttonText}
+          onClick={() => props.setAllFilter()}
+        >
+          Все
+        </ToggleButton>
+        <ToggleButton
+          value="center"
+          aria-label="centered"
+          className={styles.buttonText}
+          onClick={() => props.setActiveFilter()}
+        >
+          Активные
+        </ToggleButton>
+        <ToggleButton
+          value="right"
+          aria-label="right aligned"
+          className={styles.buttonText}
+          onClick={() => props.setComplitedFilter()}
+        >
+          Выполненные
+        </ToggleButton>
+      </ToggleButtonGroup>
+    </div>
+  );
+};
+
 
 FilterListItems.propTypes = {
   className: PropTypes.string,

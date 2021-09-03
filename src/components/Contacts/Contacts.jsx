1031dbfcdb6b02ddec688193557cc16fe91cc4d3
Anton <chiga2030@gmail.com>
2021-09-03 23:13:50 +0300

    contacts copleted

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -2,11 +2,16 @@ import {
   ReactSVG,
 } from 'react-svg';
 
+import classNames from 'classnames';
+
 import Loader from '../Loader/Loader';
 import Title from '../Title/Title';
 
 import emailIcon from './images/email.svg';
 import telegramIcon from './images/telegram.svg';
+import githubIcon from './images/github.svg';
+import linkedinIcon from './images/linkedin.svg';
+import twitterIcon from './images/twitter.svg';
 
 import styles from './Contacts.module.css';
 
@@ -35,15 +40,54 @@ const Contacts = props => (
                   className={styles.icon}
                   src={emailIcon}
                   wrapper="span" />
-                tkachuk.ant@mail.ru
+                <a href="mailto:tkachuk.ant@mail.ru">tkachuk.ant@mail.ru</a>
               </p>
               <p className={ styles.contactData }>
                 <ReactSVG
                   className={styles.icon}
                   src={telegramIcon}
                   wrapper="span" />
-                +7(926)265-4213
+                <a
+                  href={ `tg://resolve?domain=${props.contacts.items.login}` }
+                >
+                  +7(926)265-4213
+                </a>
               </p>
+            </div>
+            <div className={ styles.networks }>
+              <a href={ props.contacts.items.html_url }>
+                <ReactSVG
+                  className={classNames(
+                    styles.icon,
+                    styles.iconWithMiddleMargin,
+                  )}
+                  src={githubIcon}
+                  wrapper="span"
+                />
+              </a>
+              <a href="https://linkedin.com/in/tkachuk-anton/">
+                <ReactSVG
+                  className={classNames(
+                    styles.icon,
+                    styles.iconWithMiddleMargin,
+                  )}
+                  src={linkedinIcon}
+                  wrapper="span"
+                />
+              </a>
+              <a
+                href={ `https://twitter.com/${
+                  props.contacts.items.twitter_username}` }
+              >
+                <ReactSVG
+                  className={classNames(
+                    styles.icon,
+                    styles.iconWithMiddleMargin,
+                  )}
+                  src={twitterIcon}
+                  wrapper="span"
+                />
+              </a>
             </div>
           </div>
         }

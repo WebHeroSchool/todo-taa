4f6beec7979337f27b5e0efeb0bfc60fd33a5726
Anton <chiga2030@gmail.com>
2021-09-04 23:20:29 +0300

    refactoring Contacts component

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -11,9 +11,12 @@ import twitterIcon from './images/twitter.svg';
 import styles from './Contacts.module.css';
 
 
-const Contacts = props => (
-  props.contacts.isLoading ?
-    <Loader /> :
+const Contacts = props => {
+  if (props.contacts.isLoading) {
+    return <Loader />;
+  } else if (props.contacts.fetchReposFailure) {
+    <p>Fetch error: { props.contacts.errorMessage }</p>;
+  } return (
     <>
       <div className={ styles.contactsInner }>
 
@@ -22,55 +25,50 @@ const Contacts = props => (
           src={ props.contacts.items.avatar_url }
           alt="user avatar"
         />
+        <div className={ styles.contactsWrapper }>
+          <Title variant="h1" titleValue={ props.contacts.items.name } />
 
-        {props.contacts.fetchReposFailure ?
-          <p>Fetch error: { props.contacts.errorMessage }</p> :
-
-          <div className={ styles.contactsWrapper }>
-            <Title variant="h1" titleValue={ props.contacts.items.name } />
-
-            <div className={ styles.content }>
-              <p>{ props.contacts.items.bio }</p>
-              <SocialLink
-                iconSrc={ emailIcon }
-                linkTo="tkachuk.ant@mail.ru"
-                linkValue="tkachuk.ant@mail.ru"
-                isEmail={true}
-              />
-              <SocialLink
-                iconSrc={ telegramIcon }
-                linkTo={ `tg://resolve?domain=${props.contacts.items.login}` }
-                linkValue="+7(926)265-4213"
-              />
-            </div>
-
-            <div className={ styles.networks }>
-              <SocialLink
-                iconSrc={ githubIcon }
-                linkTo={ props.contacts.items.html_url }
-                isBlank={ true }
-                isWithMiddleMargin={ true }
-              />
-              <SocialLink
-                iconSrc={ linkedinIcon }
-                linkTo="https://linkedin.com/in/tkachuk-anton/"
-                isBlank={ true }
-                isWithMiddleMargin={ true }
-              />
-              <SocialLink
-                iconSrc={ twitterIcon }
-                linkTo={ `https://twitter.com/${
-                  props.contacts.items.twitter_username}` }
-                isBlank={ true }
-                isWithMiddleMargin={ true }
-              />
-            </div>
+          <div className={ styles.content }>
+            <p>{ props.contacts.items.bio }</p>
+            <SocialLink
+              iconSrc={ emailIcon }
+              linkTo="tkachuk.ant@mail.ru"
+              linkValue="tkachuk.ant@mail.ru"
+              isEmail={true}
+            />
+            <SocialLink
+              iconSrc={ telegramIcon }
+              linkTo={ `tg://resolve?domain=${props.contacts.items.login}` }
+              linkValue="+7(926)265-4213"
+            />
           </div>
-        }
+
+          <div className={ styles.networks }>
+            <SocialLink
+              iconSrc={ githubIcon }
+              linkTo={ props.contacts.items.html_url }
+              isBlank={ true }
+              isWithMiddleMargin={ true }
+            />
+            <SocialLink
+              iconSrc={ linkedinIcon }
+              linkTo="https://linkedin.com/in/tkachuk-anton/"
+              isBlank={ true }
+              isWithMiddleMargin={ true }
+            />
+            <SocialLink
+              iconSrc={ twitterIcon }
+              linkTo={ `https://twitter.com/${
+                props.contacts.items.twitter_username}` }
+              isBlank={ true }
+              isWithMiddleMargin={ true }
+            />
+          </div>
+        </div>
 
       </div>
     </>
-);
-
+  );
+};
 
 export default Contacts;

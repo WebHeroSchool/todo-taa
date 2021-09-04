01020738cfe78f32cb1ff55a628b752fc7285f69
Anton <chiga2030@gmail.com>
2021-09-04 23:25:26 +0300

    work to MyRepos component

--- src/components/MyRepos/MyRepos.jsx
+++ src/components/MyRepos/MyRepos.jsx
@@ -1,30 +1,34 @@
 import Loader from '../Loader/Loader';
 import Title from '../Title/Title';
+import EmptyRepos from './EmptyRepos/EmptyRepos';
 
 
-const MyRepos = props => (
-  props.repos.isLoading ?
-    <Loader /> :
+const MyRepos = props => {
+  console.log(props.repos.items.length);
+  if (props.repos.isLoading) {
+    return <Loader />;
+  } else if (props.repos.items.length === 0) {
+    return <EmptyRepos />;
+  } else if (props.repos.fetchReposFailure) {
+    return <p>{ props.repos.errorMessage }</p>;
+  } return (
     <>
       <Title variant="h2" titleValue="Репозитории на github.com" />
-      {props.repos.fetchReposFailure ?
-        <p>{ props.repos.errorMessage }</p> :
-        <ol>
-          {
-            props.repos.items.map(
-              item => (
-                <li key={item.id}>
-                  <a href={ item.html_url } target="_blank">
-                    { item.name }
-                  </a>
-                </li>
-              )
+      <ol>
+        {
+          props.repos.items.map(
+            item => (
+              <li key={item.id}>
+                <a href={ item.html_url } target="_blank">
+                  { item.name }
+                </a>
+              </li>
             )
-          }
-        </ol>
-      }
+          )
+        }
+      </ol>
     </>
-);
-
+  );
+};
 
 export default MyRepos;

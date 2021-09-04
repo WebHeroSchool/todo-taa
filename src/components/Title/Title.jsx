c5d3f6a032ffbd3718ba90963e7db94bccf66957
Anton <chiga2030@gmail.com>
2021-09-04 23:12:56 +0300

    refactoring Title component

--- src/components/Title/Title.jsx
+++ src/components/Title/Title.jsx
@@ -1,39 +1,25 @@
 import classNames from 'classnames';
-
 import styles from './Title.module.css';
 
 
-const helper = (variant, titleValue) => {
-  if (variant === 'h1') {
+const Title = ({
+  variant,
+  titleValue,
+}) => {
+  if (variant === 'h2') {
     return (
-      <h1 className={ classNames(
-        styles.title,
-        styles.h1,
-      ) }>
-        { titleValue }
-      </h1>
-    );
-  } else if (variant === 'h2') {
-    return (
-      <h2 className={ classNames(
+      <h2 className={classNames(
         styles.title,
         styles.h2,
-      ) }>
-        { titleValue }
-      </h2>
+      )}>{ titleValue }</h2>
     );
-  };
+  } return (
+    <h1 className={classNames(
+      styles.title,
+      styles.h1,
+    )}>{ titleValue }</h1>
+  );
 };
-
-
-const Title = ({
-  variant = 'h1',
-  titleValue,
-}) => (
-  <>
-    { helper(variant, titleValue) }
-  </>
-);
 
 
 export default Title;

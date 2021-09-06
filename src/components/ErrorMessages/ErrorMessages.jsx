d8e64ac82b7c82efbef1e3fe310d60e97629e761
Anton <chiga2030@gmail.com>
2021-09-06 22:56:39 +0300

    work to refresh page

--- src/components/ErrorMessages/ErrorMessages.jsx
+++ src/components/ErrorMessages/ErrorMessages.jsx
@@ -1,39 +1,40 @@
 import styles from './ErrorMessages.module.css';
 
-const errorsState = {
-  empty: {
-    errorText: 'Репозитории отсутствуют',
-    errorDescription: (
-      <>Добавьте как минимум один репозиторий на <a
-        href="https://github.com" target="blank">github.com</a></>),
-  },
-  'Not Found': {
-    errorText: 'Пользователь не найден',
-    errorDescription: 'Не могу найти данного пользователя',
-  },
-};
-
 const ErrorMessages = ({
   errorType,
 }) => {
-  const errorMessage = (
-    <>
-      <p className={ styles.errorText }>
-        { errorsState.[errorType].errorText }
-      </p>
-      <p className={ styles.errorDescription }>
-        <small>
-          { errorsState.[errorType].errorDescription }
-        </small>
-      </p>
-    </>
-  );
+  const errorsState = {
+    empty: {
+      errorText: 'Репозитории отсутствуют',
+      errorDescription: (
+        <>Добавьте как минимум один репозиторий на <a
+          href="https://github.com" target="blank">github.com</a></>),
+    },
+    'Not Found': {
+      errorText: 'Пользователь не найден',
+      errorDescription: 'Не могу найти данного пользователя',
+    },
+    'fetch error': {
+      errorText: 'Что-то пошло не так...',
+      errorDescription: (
+        <>Попробуйте <a
+          href="#">загрузить</a> ещё раз</>),
+    },
+  };
+
 
   return (
     <div className={ styles.windowWrapper }>
       <div className={ styles.background } />
       <div className={ styles.errorMessage }>
-        { errorMessage }
+        <p className={ styles.errorText }>
+          { errorsState.[errorType].errorText }
+        </p>
+        <p className={ styles.errorDescription }>
+          <small>
+            { errorsState.[errorType].errorDescription }
+          </small>
+        </p>
       </div>
     </div>
   );

f1914f7975a2f7419162c1dc3e32b41ebc277b48
Anton Tkachuk <chiga2030@gmail.com>
2021-09-17 11:10:42 +0300

    work to double click event

--- src/components/TodoItem/TodoItem.jsx
+++ src/components/TodoItem/TodoItem.jsx
@@ -8,66 +8,95 @@ import {
 import DeleteForeverTwoToneIcon from '@material-ui/icons/DeleteForeverTwoTone';
 import PropTypes from 'prop-types';
 
-let prevent = false;
-let timer;
+// let prevent = false;
+// let timer;
 
-const onSingleClickHandler = handler => {
-  timer = setTimeout(
-    () => {
-      if (!prevent) {
-        handler();
+// const onSingleClickHandler = handler => {
+//   timer = setTimeout(
+//     () => {
+//       if (!prevent) {
+//         handler();
+//       }
+//     }, 200);
+// };
+
+// const onDoubleClickHandler = handler => {
+//   clearTimeout(timer);
+//   prevent = true;
+//   handler();
+//   setTimeout(() => {
+//     prevent = false;
+//   }, 200);
+// };
+
+
+const TodoItem = props => {
+  const onClick = () => {
+    let time;
+    let timerId;
+    const delay = 200;
+
+    return function () {
+      if (!time) {
+        time = Date.now();
+        timerId = setTimeout(() => {
+          onSingleClickHandler();
+          time = null;
+        }, delay);
+      } else if (time) {
+        if ((Date.now() - time) < delay) {
+          clearTimeout(timerId);
+          onDoubleClickHandler();
+        };
+        time = null;
       }
-    }, 200);
+    };
+  };
+
+  const onHandleClick = onClick();
+
+  const onSingleClickHandler = () => props.onChangeCheckbox();
+  const onDoubleClickHandler = () => props.onEditItem();
+  console.log();
+  return (
+    <span className = {
+      classnames({
+        [styles.item]: true,
+        [styles.done]: props.isDone,
+      })
+    }>
+      <FormControlLabel
+        control={
+          <Checkbox
+            style={{
+              alignSelf: 'flex-start',
+            }}
+            checked={ props.isDone }
+            name={ `checked ${props.value.toString()}` }
+          />
+        }
+        label={
+          <span
+            contentEditable={ props.isEditable }
+            suppressContentEditableWarning={ true }
+          >
+            { props.value }
+          </span>
+        }
+        onClick={ () => onHandleClick() }
+      />
+      <Button
+        className={ styles.deleteBtn }
+        variant="contained"
+        color="secondary"
+        onClick={ props.onClickButton }
+      >
+        <DeleteForeverTwoToneIcon fontSize="medium" />
+      </Button>
+    </span>
+  );
 };
 
-const onDoubleClickHandler = handler => {
-  clearTimeout(timer);
-  prevent = true;
-  handler();
-  setTimeout(() => {
-    prevent = false;
-  }, 200);
-};
-
-
-const TodoItem = props => (
-  <span className = {
-    classnames({
-      [styles.item]: true,
-      [styles.done]: props.isDone,
-    })
-  }>
-    <FormControlLabel
-      control={
-        <Checkbox
-          style={{
-            alignSelf: 'flex-start',
-          }}
-          onClick={ () => onSingleClickHandler(props.onChangeCheckbox) }
-          checked={ props.isDone }
-          name={ `checked ${props.value.toString()}` }
-        />
-      }
-      label={
-        <span
-          contentEditable={ props.isEditable }
-          suppressContentEditableWarning={ true }
-          onDoubleClick={ () => onDoubleClickHandler(props.onEditItem) }
-        >
-          { props.value }
-        </span>
-      }
-    />
-    <Button
-      className={ styles.deleteBtn }
-      variant="contained"
-      color="secondary"
-      onClick={ props.onClickButton }
-    >
-      <DeleteForeverTwoToneIcon fontSize="medium" />
-    </Button>
-  </span>
-);
 
 TodoItem.propTypes = {
   id: PropTypes.number,

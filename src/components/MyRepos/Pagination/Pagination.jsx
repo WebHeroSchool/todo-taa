c36192926a65bc72e679a2b7e1c832f3cd23c3cc
Anton Tkachuk <chiga2030@gmail.com>
2021-10-01 11:56:01 +0300

    refactoring pagination component

--- src/components/MyRepos/Pagination/Pagination.jsx
+++ src/components/MyRepos/Pagination/Pagination.jsx
@@ -1,24 +1,21 @@
 import styles from './Pagination.module.css';
 
-const inputs = length => {
-  const result = new Array(Math.ceil(length / 8));
+const getPagesNum = length => {
+  const result = new Array(Number(length));
   for (let index = 0; index < result.length; index++) {
     result[index] = index + 1;
   };
-  console.log(result);
   return result;
 };
-// { Math.ceil(length / 8) }
 
 const Pagination = ({
   length,
 }) => (
   <div className={ styles.pagination }>
-    {length}
     <input type="button" value="<" />
     {
-      inputs(length).map(item => (
-        <input type="button" value={item} />
+      getPagesNum(length).map(item => (
+        <input key={item} type="button" value={item} />
       ))
     }
     <input type="button" value=">" />

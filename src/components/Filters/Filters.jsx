dd8506a3f92de7efda072f1b0659ec69bbb77550
Anton <chiga2030@gmail.com>
2021-10-13 22:06:48 +0300

    refactoring of filters component

--- src/components/Filters/Filters.jsx
+++ src/components/Filters/Filters.jsx
@@ -1,8 +1,6 @@
 import styles from './Filters.module.css';
+// import classNames from 'classnames';
 
-import {
-  ReactComponent as ReverseSvg,
-} from './images/reverse.svg';
 import {
   ReactComponent as AlphaSvg,
 } from './images/alpha-desc.svg';
@@ -10,20 +8,27 @@ import {
   ReactComponent as DateSvg,
 } from './images/date-desc.svg';
 import {
+  ReactComponent as ReverseSvg,
+} from './images/reverse.svg';
+import {
   ReactComponent as CancelSvg,
 } from './images/cancel.svg';
 
+const filters = [
+  AlphaSvg,
+  DateSvg,
+  ReverseSvg,
+];
+
 const Filters = () => (
   <div className={ styles.wrapper }>
-    <button className={ styles.sortButton } >
-      <AlphaSvg className={ styles.buttonImage } />
-    </button>
-    <button className={ styles.sortButton } >
-      <DateSvg className={ styles.buttonImage } />
-    </button>
-    <button className={ styles.sortButton } >
-      <ReverseSvg className={ styles.buttonImage } />
-    </button>
+    {
+      filters.map((ButtonImg, index) => (
+        <button key={ index } className={ styles.sortButton } >
+          <ButtonImg className={ styles.buttonImage } />
+        </button>
+      ))
+    }
     <CancelSvg />
   </div>
 );

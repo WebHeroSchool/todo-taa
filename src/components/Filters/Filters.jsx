486ce1688de5454eac0a5fe8d79fe9e387c6a31c
Anton Tkachuk <chiga2030@gmail.com>
2021-10-15 09:10:49 +0300

    view of filters button

--- src/components/Filters/Filters.jsx
+++ src/components/Filters/Filters.jsx
@@ -1,5 +1,5 @@
 import styles from './Filters.module.css';
-// import classNames from 'classnames';
+import classNames from 'classnames';
 
 import {
   ReactComponent as AlphaSvg,
@@ -14,24 +14,64 @@ import {
   ReactComponent as CancelSvg,
 } from './images/cancel.svg';
 
-const filters = [
-  AlphaSvg,
-  DateSvg,
-  ReverseSvg,
+import {
+  useState,
+} from 'react';
+
+
+const initialFilters = [
+  {
+    'icon': AlphaSvg,
+    'isActive': false,
+  },
+  {
+    'icon': DateSvg,
+    'isActive': false,
+  },
+  {
+    'icon': ReverseSvg,
+    'isActive': false,
+  },
 ];
 
-const Filters = () => (
-  <div className={ styles.wrapper }>
-    {
-      filters.map((ButtonImg, index) => (
-        <button key={ index } className={ styles.sortButton } >
-          <ButtonImg className={ styles.buttonImage } />
-        </button>
-      ))
-    }
-    <CancelSvg />
-  </div>
-);
+const Filters = () => {
+  const onClickHandler = id => setFilters(
+    filters.map(item => {
+      if (item.icon === id) {
+        item.isActive = true;
+      } else {
+        item.isActive = false;
+      }
+
+      return item;
+    })
+  );
+
+  const [
+    filters,
+    setFilters,
+  ] = useState(initialFilters);
+
+  return (
+    <div className={ styles.wrapper }>
+      {
+        filters.map((Button, index) => (
+          <button
+            key={ index }
+            className={ styles.sortButton }
+            onClick={ () => onClickHandler(Button.icon) }
+          >
+            <Button.icon className={ classNames({
+              [styles.buttonImage]: true,
+              [styles.active]: Button.isActive,
+            }) } />
+          </button>
+        ))
+      }
+      <CancelSvg />
+    </div>
+  );
+};
 
 
 export default Filters;

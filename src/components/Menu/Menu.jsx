6951006b586aa00fb43054742552750f555b2d76
Anton <chiga2030@gmail.com>
2022-01-15 14:01:34 +0300

    mend

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,17 +1,19 @@
-import styles from './Menu.module.css';
-
 import {
-  React,
+  Suspense,
+  lazy,
   useState,
 } from 'react';
-import {
-  Paper,
-  MenuList,
-  Divider,
-} from '@material-ui/core';
 
-import AppLink from '../AppLink/AppLink';
-import Label from '../Label/Label';
+import styles from './Menu.module.css';
+
+
+const Paper = lazy(() => import('@material-ui/core/Paper'));
+const MenuList = lazy(() => import('@material-ui/core/MenuList'));
+const Divider = lazy(() => import('@material-ui/core/Divider'));
+
+const Loader = lazy(() => import('../Loader/Loader'));
+const AppLink = lazy(() => import('../AppLink/AppLink'));
+const Label = lazy(() => import('../Label/Label'));
 
 
 const Menu = () => {
@@ -23,27 +25,29 @@ const Menu = () => {
 
   return (
     <Paper className={styles.menu} elevation={3} >
-      <MenuList>
-        <nav className={ styles.menuList }>
-          <AppLink
-            to="/"
-            selectedIndex={selectedIndex}
-            setSelectedIndex={setSelectedIndex}
-            primary="Обо мне"
-          />
+      <Suspense fallback={<Loader />}>
+        <MenuList>
+          <nav className={ styles.menuList }>
+            <AppLink
+              to="/"
+              selectedIndex={selectedIndex}
+              setSelectedIndex={setSelectedIndex}
+              primary="Обо мне"
+            />
 
-          <AppLink
-            to="/todo"
-            selectedIndex={selectedIndex}
-            setSelectedIndex={setSelectedIndex}
-            primary="Дела"
-          />
-        </nav>
-      </MenuList>
+            <AppLink
+              to="/todo"
+              selectedIndex={selectedIndex}
+              setSelectedIndex={setSelectedIndex}
+              primary="Дела"
+            />
+          </nav>
+        </MenuList>
 
-      <Divider className={ styles.divider } />
+        <Divider className={ styles.divider } />
 
-      <Label />
+        <Label />
+      </Suspense>
     </Paper>
   );
 };

dd0f2b09f31cf792a019f32b431f89c5db396ed8
Anton <chiga2030@gmail.com>
2022-01-16 23:31:23 +0300

    added open graph layout

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -3,6 +3,8 @@ import {
   lazy,
 } from 'react';
 
+import Helmet from 'react-helmet';
+
 import withIndent from '../hoc/withIndent/withIndent';
 import withMargin from '../hoc/withMargin/withMargin';
 import withScrollBar from '../hoc/withScrollBar/withScrollBar';
@@ -30,24 +32,54 @@ const ItemListContainerWithScrollbar = props => withScrollBar(
   ItemListContainer, props,
   props.todos.items[0].filter, props.todos.items[0].id);
 
+const meta = {
+  title: 'ToDo App',
+  description: 'Веб-приложение - список дел',
+  image: {
+    url: 'https://webheroschool.github.io/todo-taa/toDoApp.png',
+    alt: 'ToDo App',
+    width: '1200',
+    height: '846',
+  },
+  url: 'https://webheroschool.github.io/todo-taa/#/todo',
+  type: 'website',
+  id: '488242212639151',
+};
+
+
 const Todo = ({
   count,
   ...props
 }) => (
-  <Suspense fallback={<Loader />}>
-    <Title titleValue={ 'TO DO:' } />
-    <InputItemContainer />
-    {
-      count > 0 ?
-        <>
-          <SortContainer />
-          <ItemListContainerWithScrollbar { ...props } />
-        </> :
-        <ErrorMessagesWithMargin errorType="empty todoList" />
-    }
-    <Divider />
-    <FooterWithIdent />
-  </Suspense>
+  <>
+    <Helmet>
+      <title>{ meta.title }</title>
+      <meta name="description" content={ meta.description }/>
+      <meta property="og:title" content={ meta.title }/>
+      <meta property="og:image" content={ meta.image.url }/>
+      <meta property="og:image:alt" content={ meta.image.alt }/>
+      <meta property="og:image:width" content={ meta.image.width }/>
+      <meta property="og:image:height" content={ meta.image.height }/>
+      <meta property="og:url" content={ meta.url } />
+      <meta property="og:type" content={ meta.type } />
+      <meta property="og:description" content={ meta.description } />
+      <meta property="fb:app_id" content={ meta.id } />
+    </Helmet>
+    <Suspense fallback={<Loader />}>
+      <Title titleValue={ 'TO DO:' } />
+      <InputItemContainer />
+      {
+        count > 0 ?
+          <>
+            <SortContainer />
+            <ItemListContainerWithScrollbar { ...props } />
+          </> :
+          <ErrorMessagesWithMargin errorType="empty todoList" />
+      }
+      <Divider />
+      <FooterWithIdent />
+    </Suspense>
+  </>
 );
 
 
--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,8 @@ import {
   lazy,
 } from 'react';
 
+import Helmet from 'react-helmet';
+
 import {
   Provider,
 } from 'react-redux';
@@ -15,11 +17,40 @@ import ReactDOM from 'react-dom';
 
 const App = lazy(() => import('./components/App/App'));
 
+const meta = {
+  title: 'Обо мне',
+  description: 'Информация обо мне, мои контакты и репозитории на GitHub',
+  image: {
+    url: 'https://webheroschool.github.io/todo-taa/aboutMe.jpg',
+    alt: 'networking',
+    width: '1200',
+    height: '628',
+  },
+  url: 'https://webheroschool.github.io/todo-taa/#/',
+  type: 'website',
+  id: '488242212639151',
+};
+
 
 ReactDOM.render(
-  <Suspense fallback={<div>Loading...</div>}>
-    <Provider store={store}>
-      <App/>
-    </Provider>
-  </Suspense>
+  <>
+    <Helmet>
+      <title>{ meta.title }</title>
+      <meta name="description" content={ meta.description }/>
+      <meta property="og:title" content={ meta.title }/>
+      <meta property="og:image" content={ meta.image.url }/>
+      <meta property="og:image:alt" content={ meta.image.alt }/>
+      <meta property="og:image:width" content={ meta.image.width }/>
+      <meta property="og:image:height" content={ meta.image.height }/>
+      <meta property="og:url" content={ meta.url } />
+      <meta property="og:type" content={ meta.type } />
+      <meta property="og:description" content={ meta.description } />
+      <meta property="fb:app_id" content={ meta.id } />
+    </Helmet>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Provider store={store}>
+        <App/>
+      </Provider>
+    </Suspense>
+  </>
   , document.getElementById('root'));
